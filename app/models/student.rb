096e21f9ca9dd9c045d3f81a17d1aac5defb5bbe
andrewgrablewski <agrablewski@gmail.com>
2020-10-04 11:54:20 -0400

    Enroll in courses from ui

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,6 +7,8 @@ class Student < ApplicationRecord
                                    length: { maximum: 105},
                                    format: {with:VALID_EMAIL_REGEX }
   has_secure_password
+  has_many :student_courses
+  has_many :courses, through: :student_courses
 
 
 
